671c1ac60e25260c0fbc6a061b11f90334bb47f4
dzosz <tomek3319@gmail.com>
2021-05-16 20:37:49 +0200

    dft use vector instead arrays

--- rust_algo/distrete_fourier_transform/src/main.rs
+++ rust_algo/distrete_fourier_transform/src/main.rs
@@ -1,10 +1,10 @@
 extern crate num;
 
 use num::Complex;
+use std::env;
 
-const LEN: usize = 4;
-
-type OutputT = [Complex<f64>; LEN];
+type InputT = Vec<i32>;
+type OutputT = Vec<Complex<f64>>;
 
 
 // The sine and cosine waves are called DFT basic functions - they are waves with unity amplitude. The DFT basic functions have the following equations:
@@ -12,25 +12,33 @@ type OutputT = [Complex<f64>; LEN];
 // sk[i] = sin(2pi * k * i/N)
 // x[i]  = sum(ck) + i * sum(sk)
 
-fn transform_using_waves(k : usize, j : usize) -> Complex<f64>{
-    let common = (2*k * j) as f64 * std::f64::consts::PI * -1.0 / LEN as f64;
+fn transform_using_waves(k : usize, j : usize, n : usize) -> Complex<f64>{
+    let common = (2*k * j) as f64 * std::f64::consts::PI * -1.0 / n as f64;
     Complex::new(common.cos(), common.sin())
 }
 
-fn calculate_dft(input: [i32; LEN]) -> OutputT {
-    let mut output: OutputT = Default::default();
+fn calculate_dft(input: InputT) -> OutputT {
+    let mut output: OutputT = Vec::new();
+    output.resize_with(input.len(), Default::default);
 
     for (k, elem) in output.iter_mut().enumerate() {
-        for j in 0..LEN {
-            *elem += transform_using_waves(k, j) * input[j] as f64;
+        for j in 0..input.len() {
+            *elem += transform_using_waves(k, j, input.len()) * input[j] as f64;
         }
-        println!("fk^ {} : {}", k, elem);
     }
     output
 }
 
+fn get_optional_user_input() -> InputT {
+    let args : Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        return args.iter().skip(1).map(|val| val.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    }
+    return vec![1,2,3,4];
+}
+
 fn main() {
-    let input: [i32; LEN] = [1, 2, 3, 4];
+    let input = get_optional_user_input();
     println!("Input: {:?}", input);
     let output = calculate_dft(input);
     println!("Output: {:?}", output);
