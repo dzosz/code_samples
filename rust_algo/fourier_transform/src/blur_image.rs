fffb0c1855514bfadc39dc8c45c65dfbc383e725
dzosz <tomek3319@gmail.com>
2021-05-26 03:07:36 +0200

    add two dimensional fft, blurring image now works

--- rust_algo/fourier_transform/src/blur_image.rs
+++ rust_algo/fourier_transform/src/blur_image.rs
@@ -1,2 +1,136 @@
+extern crate ft_by_hand;
+extern crate nalgebra as na;
+extern crate num;
+extern crate png;
+
+pub use num::Complex;
+use std::env;
+use std::fs::File;
+use std::io::BufWriter;
+use std::path::Path;
+
+use ft_by_hand::*;
+
+fn get_user_filename() -> String {
+    let args: Vec<String> = env::args().collect();
+    assert!(
+        args.len() == 2,
+        "no path to png file passed in program arguments"
+    );
+    args[1].parse::<String>().unwrap()
+}
+
+fn write_file(fname: String, data: &[u8], width: u32, height: u32) {
+    println!("writing file {}", fname);
+
+    let path = Path::new(&fname);
+    let file = File::create(path).unwrap();
+    let ref mut w = BufWriter::new(file);
+
+    let mut encoder = png::Encoder::new(w, height, width);
+    encoder.set_color(png::ColorType::Grayscale);
+    encoder.set_depth(png::BitDepth::Eight);
+    let mut writer = encoder.write_header().unwrap();
+
+    writer.write_image_data(&data).unwrap(); // Save
+}
+
+fn rgba_to_grayscale(img_src: &[u8]) -> Vec<u8> {
+    let mut result = Vec::new();
+    for i in (0..img_src.len()).step_by(4) {
+        let rgba_row = &img_src[i..i + 4];
+        let luminosity =
+            0.299 * rgba_row[0] as f64 + 0.587 * rgba_row[1] as f64 + 0.114 * rgba_row[2] as f64;
+        result.push(luminosity as u8);
+    }
+    result
+}
+
+#[allow(dead_code)]
+fn transform_magnitude_spectrum(input: &DataT) -> DataT {
+    input
+        .iter()
+        .map(|&val| {
+            Complex::new(
+                20. * ((val.im * val.im) + (val.re + val.re)).sqrt().ln(),
+                0.,
+            )
+        })
+        .collect::<DataT>()
+}
+
+fn blur_image(img_src: &DataT) -> DataT {
+    let side = (img_src.len() as f64).sqrt().round() as usize;
+
+    let mask_size = side;
+    let mut mask =
+        na::DMatrix::<Complex<f64>>::from_element(mask_size, mask_size, Default::default());
+
+    let blur_size = mask_size / 5;
+    let start = (mask_size / 2) - (blur_size / 2);
+    for i in 0..blur_size {
+        for j in 0..blur_size {
+            mask[(i + start, j + start)] = Complex::new(1.0, 1.0);
+        }
+    }
+
+    // some implementations say we should fft mask first
+    /*
+    unsafe {
+        *mask.data.as_vec_mut() = calculate_fft2(&mask.data.as_vec().clone());
+    }
+
+    for i in 0..blur_size {
+        for j in 0..blur_size {
+            if mask[(i+start,j+start)].im == 0. {
+                mask[(i+start,j+start)].im = 1e-6;
+            }
+            if mask[(i+start,j+start)].re == 0. {
+                mask[(i+start,j+start)].re = 1e-6;
+            }
+        }
+    }*/
+
+    let img = na::Matrix::from_vec_generic(
+        na::Dynamic::new(side),
+        na::Dynamic::new(side),
+        img_src.clone(),
+    );
+    let blurred = img.component_mul(&mask);
+
+    blurred.data.as_vec().clone()
+}
+
 fn main() {
+    let fname = get_user_filename();
+    let decoder = png::Decoder::new(File::open(fname.clone()).unwrap());
+    let (info, mut reader) = decoder.read_info().unwrap();
+    let mut buf = vec![0; info.buffer_size()];
+    reader.next_frame(&mut buf).unwrap();
+
+    let (width, height) = (info.height, info.width);
+
+    println!(
+        "image w={} h={} colortype={:?}",
+        width, height, info.color_type
+    );
+    assert!(
+        info.color_type == png::ColorType::RGBA,
+        "image must be RGBA"
+    );
+
+    let gray_image_complex: DataT = rgba_to_grayscale(&buf)
+        .iter()
+        .map(|val| Complex::new(*val as f64, 0.0))
+        .collect();
+
+    let blurred_gray_image = calculate_ifft2(&ifft_shift(
+        &blur_image(&fft_shift(&calculate_fft2(&gray_image_complex), 2)),
+        2,
+    ))
+    .iter()
+    .map(|val| val.re as u8)
+    .collect::<Vec<u8>>();
+
+    write_file(fname.clone() + "2", &blurred_gray_image[..], width, height);
 }
--- rust_algo/fourier_transform/src/ft_by_hand.rs
+++ rust_algo/fourier_transform/src/ft_by_hand.rs
@@ -6,6 +6,11 @@ fn is_power_of_two(n: usize) -> bool {
     n != 0 && (n & (n - 1) == 0)
 }
 
+enum FftType {
+    Normal,
+    Inverse,
+}
+
 // The sine and cosine waves are called DFT basic functions - they are waves with unity amplitude. The DFT basic functions have the following equations:
 // By eulers formula e^(i*theta) = cos(theta) + i * sin(theta)
 // ck[i] = cos(2pi * k * i/N)
@@ -19,7 +24,7 @@ fn polar(magnitude: f64, phase_angle: f64) -> Complex<f64> {
 // Cooley–Tukey FFT (in-place, divide-and-conquer)
 // Higher memory requirements and redundancy although more intuitive
 // adapted from c++ code from https://rosettacode.org/wiki/Fast_Fourier_transform
-fn _calculate_fft(input: &mut DataT) {
+fn _calculate_fft(input: &mut DataT, t: &FftType) {
     let n = input.len();
     if n <= 1 {
         return;
@@ -30,12 +35,19 @@ fn _calculate_fft(input: &mut DataT) {
     let mut odd = input.iter().skip(1).step_by(2).copied().collect();
 
     // conquer
-    _calculate_fft(&mut even);
-    _calculate_fft(&mut odd);
+    _calculate_fft(&mut even, &t);
+    _calculate_fft(&mut odd, &t);
 
+    let direction = match t {
+        FftType::Inverse => 1.0,
+        _ => -1.0,
+    };
     // combine
     for k in 0..n / 2 {
-        let t = polar(1.0, -2.0 * k as f64 * std::f64::consts::PI / n as f64) * odd[k];
+        let t = polar(
+            1.0,
+            direction * 2.0 * k as f64 * std::f64::consts::PI / n as f64,
+        ) * odd[k];
         input[k] = even[k] + t;
         input[k + n / 2] = even[k] - t;
     }
@@ -56,6 +68,50 @@ impl PartialEq for ComplexComparatorWrapper<'_> {
 impl Eq for ComplexComparatorWrapper<'_> {}
 
 pub type DataT = Vec<Complex<f64>>;
+
+//  rearranges a Fourier transform X by shifting the zero-frequency component to the center of the array.
+//  For vectors, fftshift(X) swaps the left and right halves of X.
+//  For matrices, fftshift(X) swaps quadrants one and three of X with quadrants two and four.
+//  For higher-dimensional arrays, fftshift(X) swaps "half-spaces" of X along each dimension.
+pub fn fft_shift(input: &DataT, dim: usize) -> DataT {
+    let mut output = input.clone();
+    let size = input.len();
+    let side = (size as f64).sqrt().round() as usize;
+
+    match dim {
+        1 |2 => {
+            output.rotate_right(size/2);
+            output.chunks_mut(side).for_each(|chunk| chunk.rotate_right(side/2));
+            /*
+            for src in 0..size {
+                let dst = (src + (size / 2) - 1) % size;
+                output.swap(src, dst);
+                println!("swap {}->{}", src, dst);
+            }*/
+        }
+        _ => {
+            panic!("fft higher shift dimension={} are not handled", dim);
+        }
+    }
+    output
+}
+
+pub fn ifft_shift(input: &DataT, dim: usize) -> DataT {
+    let mut output = input.clone();
+    let size = input.len();
+    let side = (size as f64).sqrt().round() as usize;
+
+    match dim {
+        1 |2 => {
+            output.rotate_left(size/2);
+            output.chunks_mut(side).for_each(|chunk| chunk.rotate_left(side/2));
+        }
+        _ => {
+            panic!("fft higher shift dimension={} are not handled", dim);
+        }
+    }
+    output
+}
 
 pub fn calculate_dft(input: &DataT) -> DataT {
     let mut output: DataT = Vec::new();
@@ -88,6 +144,19 @@ pub fn calculate_idft(input: &DataT) -> DataT {
     output
 }
 
+fn transpose(input: &DataT) -> DataT {
+    let mut output = input.clone();
+    let side = (input.len() as f64).sqrt().round() as usize;
+    for i in 0..side {
+        for j in i..side {
+            let left = i + j*side;
+            let right = i*side + j;
+            output.swap(left, right);
+        }
+    }
+    output
+}
+
 pub fn calculate_fft(input: &DataT) -> DataT {
     assert!(
         is_power_of_two(input.len()),
@@ -95,7 +164,75 @@ pub fn calculate_fft(input: &DataT) -> DataT {
     );
     let mut output = input.clone();
 
-    _calculate_fft(&mut output);
+    _calculate_fft(&mut output, &FftType::Normal);
+    output
+}
+
+// TODO there is better algorithm for that
+pub fn calculate_fft2(input: &DataT) -> DataT {
+    assert!(
+        is_power_of_two(input.len()),
+        "this fft algorithm requires input size of power of 2"
+    );
+    let mut output = input.clone();
+
+    let side = (input.len() as f64).sqrt().round() as usize;
+    for i in (0..input.len()).step_by(side) {
+        let mut tmp = output[i..i+side].to_vec();
+        _calculate_fft(&mut tmp, &FftType::Normal);
+        output.splice(i..i+side, tmp);
+    }
+    output = transpose(&mut output);
+    for i in (0..input.len()).step_by(side) {
+        let mut tmp = output[i..i+side].to_vec();
+        _calculate_fft(&mut tmp, &FftType::Normal);
+        output.splice(i..i+side, tmp);
+    }
+    output = transpose(&mut output);
+
+    output
+}
+
+pub fn calculate_ifft(input: &DataT) -> DataT {
+    assert!(
+        is_power_of_two(input.len()),
+        "this fft algorithm requires input size of power of 2"
+    );
+    let mut output = input.clone();
+
+    _calculate_fft(&mut output, &FftType::Inverse);
+    for elem in output.iter_mut() {
+        *elem = elem.unscale(input.len() as f64);
+    }
+    output
+}
+
+pub fn calculate_ifft2(input: &DataT) -> DataT {
+    assert!(
+        is_power_of_two(input.len()),
+        "this fft algorithm requires input size of power of 2"
+    );
+    let mut output = input.clone();
+
+    // by row
+    let side = (input.len() as f64).sqrt().round() as usize;
+    for i in (0..input.len()).step_by(side) {
+        let mut tmp = output[i..i+side].to_vec();
+        _calculate_fft(&mut tmp, &FftType::Inverse);
+        output.splice(i..i+side, tmp);
+    }
+    output = transpose(&mut output);
+    // by column
+    for i in (0..input.len()).step_by(side) {
+        let mut tmp = output[i..i+side].to_vec();
+        _calculate_fft(&mut tmp, &FftType::Inverse);
+        output.splice(i..i+side, tmp);
+    }
+    for elem in output.iter_mut() {
+        *elem = elem.unscale(input.len() as f64);
+    }
+    output = transpose(&mut output);
+
     output
 }
 
@@ -107,21 +244,119 @@ pub fn eq_complex_vector(left: &DataT, right: &DataT) -> bool {
 mod tests {
     use super::*;
 
-    fn generate_complex_values() -> DataT {
-        return vec![
-            Complex { re: 1.0, im: 0.0 },
-            Complex { re: 2.0, im: 0.0 },
-            Complex { re: 3.0, im: 0.0 },
-            Complex { re: 4.0, im: 0.0 },
-        ]; // default
+    fn generate_n_complex_values(n: usize) -> DataT {
+        (1..n + 1).map(|v| Complex::new(v as f64, 0.0)).collect()
     }
 
     #[test]
     fn dft_and_fft_outputs_same_values() {
-        let input = generate_complex_values();
+        let input = generate_n_complex_values(4);
         let dft_output = calculate_dft(&input);
         let fft_output = calculate_fft(&input);
         let equal_dft_fft = eq_complex_vector(&dft_output, &fft_output);
         assert!(equal_dft_fft, "computations from FFT and DFT are not equal")
+    }
+
+    #[test]
+    fn idft_and_ifft_outputs_same_values() {
+        let input = generate_n_complex_values(4);
+        let idft_output = calculate_idft(&input);
+        let ifft_output = calculate_ifft(&input);
+        let equal_dft_fft = eq_complex_vector(&idft_output, &ifft_output);
+        assert!(
+            equal_dft_fft,
+            "computations from iFFT and iDFT are not equal"
+        )
+    }
+
+    #[test]
+    fn fft_shift_simple_vector() {
+        let input = generate_n_complex_values(4);
+        let expected = vec![3, 4, 1, 2]
+            .into_iter()
+            .map(|val| Complex::new(val as f64, 0.))
+            .collect::<DataT>();
+        let result = fft_shift(&input, 1);
+        assert!(
+            eq_complex_vector(&result, &expected),
+            "Not equal!\nexp={:?}\nres={:?}",
+            expected,
+            result
+        )
+    }
+
+    #[test]
+    fn fft_shift_simple_matrix() {
+        let input = generate_n_complex_values(4);
+        let expected = vec![3, 4, 1, 2]
+            .into_iter()
+            .map(|val| Complex::new(val as f64, 0.))
+            .collect::<DataT>();
+        let result = fft_shift(&input, 2);
+        assert!(
+            eq_complex_vector(&result, &expected),
+            "Not equal!\nexp={:?}\nres={:?}",
+            expected,
+            result
+        )
+    }
+
+    #[test]
+    fn ifft_shift_simple_vector() {
+        let input = generate_n_complex_values(4);
+        let result = ifft_shift(&fft_shift(&input, 1),1);
+        assert!(
+            eq_complex_vector(&result, &input),
+            "Not equal!\nexp={:?}\nres={:?}",
+            input,
+            result
+        )
+    }
+
+    #[test]
+    fn ifft_shift_simple_matrix() {
+        let input = generate_n_complex_values(4);
+        let result = ifft_shift(&fft_shift(&input, 2),2);
+        assert!(
+            eq_complex_vector(&result, &input),
+            "Not equal!\nexp={:?}\nres={:?}",
+            input,
+            result
+        )
+    }
+    #[test]
+    fn transpose_matrix() {
+        let input = generate_n_complex_values(4);
+        let expected = vec![1, 3, 2, 4]
+            .into_iter()
+            .map(|val| Complex::new(val as f64, 0.))
+            .collect::<DataT>();
+        let result = transpose(&input);
+        assert!(
+            eq_complex_vector(&result, &expected),
+            "transpose failed\n{:?}\n{:?}", result, expected
+        )
+    }
+    #[test]
+    fn fft2_matrix() {
+        let input = generate_n_complex_values(4);
+        let expected = vec![10, -2, -4, 0]
+            .into_iter()
+            .map(|val| Complex::new(val as f64, 0.))
+            .collect::<DataT>();
+        let result = calculate_fft2(&input);
+        assert!(
+            eq_complex_vector(&result, &expected),
+            "fft2 matrix failed\nres={:?}\nexp={:?}", result,expected
+        )
+    }
+    #[test]
+    fn ifft2_matrix() {
+        let input = generate_n_complex_values(4);
+        let result = calculate_ifft2(&calculate_fft2(&input));
+        assert!(
+            eq_complex_vector(&result, &input),
+            "fft2 matrix failed\nres={:?}\nexp={:?}", result, input
+        )
     }
 }
